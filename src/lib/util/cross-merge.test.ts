60a8f1914db3dfcb43db91e87a50a5e97a684fe5
Garrett Myrick <garrettmyrick@gmail.com>
2023-06-21 11:25:23 -0700

    added tests for crossMerge

---
+++ src/lib/util/cross-merge.test.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect } from 'vitest';
+import { crossMerge, crossMergeGenerator } from './cross-merge';
+
+describe('util', () => {
+
+  const left = [{ a: 1 }, { a: 2 }];
+  const right = [{ b: 1 }, { b: 2 }];
+  const expected = [
+    { a: 1, b: 1 },
+    { a: 1, b: 2 },
+    { a: 2, b: 1 },
+    { a: 2, b: 2 }
+  ];
+
+  describe('crossMerge', () => {
+    test('returns the expected result', () => {
+      const result = crossMerge(left, right);
+      expect(result).toMatchObject(expected);
+    });
+  });
+
+  describe('crossMergeGenerator', () => {
+    test('returns the expected result', () => {
+      const result = Array.from(crossMergeGenerator(left, right));
+      expect(result).toMatchObject(expected);
+    });
+  });
+})
--- src/lib/util/cross-merge.ts
+++ src/lib/util/cross-merge.ts
@@ -7,16 +7,7 @@
  * @returns an array of merged items
  */
 export function crossMerge<TLeft extends object, TRight extends object = object>(itemsLeft: any[], itemsRight: any[]): (TRight & TLeft)[] {
-  const result: any[] = [];
-
-  for (const itemLeft of itemsLeft) {
-    for (const itemRight of itemsRight) {
-      result.push({ ...itemLeft, ...itemRight });
-    }
-  }
-
-  const set = new Set(result.map(item => JSON.stringify(item)));
-  return Array.from(set).map(item => JSON.parse(item));
+  return Array.from(crossMergeGenerator<TLeft, TRight>(itemsLeft, itemsRight));
 }
 
 
